af7ad9a9dc0e85a57a16f63d4ce8ad46509d7cd1
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2025-05-23 22:45:55 +0900

    feat: R2dbc + Kotlin DateTime 예제

---
+++ 06-advanced/03-exposed-r2dbc-kotlin-datetime/README.md
@@ -0,0 +1,3 @@
+# 03 R2dbc Kotlin DateTime example
+
+Exposed R2dbc 로 `exposed-kotlin-datetime` 모듈을 사용하여 Kotlin DataTime 관련 수형을 사용하는 방법에 대해 설명합니다. 
---
+++ 06-advanced/03-exposed-r2dbc-kotlin-datetime/src/test/kotlin/exposed/r2dbc/examples/kotlin/datetime/KotlinDateTimeSupports.kt
@@ -0,0 +1,14 @@
+package exposed.r2dbc.examples.kotlin.datetime
+
+import kotlinx.datetime.Clock
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import org.jetbrains.exposed.v1.core.CustomFunction
+import org.jetbrains.exposed.v1.datetime.KotlinOffsetDateTimeColumnType
+import java.time.OffsetDateTime
+
+
+fun now() = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
+
+internal val dbTimestampNow: CustomFunction<OffsetDateTime>
+    get() = object: CustomFunction<OffsetDateTime>("now", KotlinOffsetDateTimeColumnType()) {}
