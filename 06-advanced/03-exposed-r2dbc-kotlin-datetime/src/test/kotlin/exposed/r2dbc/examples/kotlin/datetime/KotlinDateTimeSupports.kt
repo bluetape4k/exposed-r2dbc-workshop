d935d4c87f7ae0a51b9dfb855e402a4a4b62bf63
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2025-08-05 23:57:28 +0900

    build(dep): Kotlin 시간 API 관련 Clock 임포트 수정

--- 06-advanced/03-exposed-r2dbc-kotlin-datetime/src/test/kotlin/exposed/r2dbc/examples/kotlin/datetime/KotlinDateTimeSupports.kt
+++ 06-advanced/03-exposed-r2dbc-kotlin-datetime/src/test/kotlin/exposed/r2dbc/examples/kotlin/datetime/KotlinDateTimeSupports.kt
@@ -1,11 +1,14 @@
+@file:OptIn(ExperimentalTime::class)
+
 package exposed.r2dbc.examples.kotlin.datetime
 
-import kotlinx.datetime.Clock
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 import org.jetbrains.exposed.v1.core.CustomFunction
 import org.jetbrains.exposed.v1.datetime.KotlinOffsetDateTimeColumnType
 import java.time.OffsetDateTime
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
 
 
 fun now() = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
