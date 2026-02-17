0de9626b1cbc9e1814570f6cf9b770e286de5c51
Sunghyouk Bae <sunghyouk.bae@gmail.com>
2026-02-18 00:17:39 +0900

    build: 빌드 작업을 병렬로 수행하도록 수정
     - 테스트를 제외한 작업을 병렬로 수행
     - 테스트 작업은 한 모듈씩 수행하도록 Mutex 로 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -116,7 +116,26 @@ subprojects {
             options.isIncremental = true
         }
 
+        compileKotlin {
+            compilerOptions {
+                incremental = true
+            }
+        }
+
+        // 멀티 모듈들을 테스트 시에만 동시에 실행되지 않게 하기 위해 Mutex 를 활용합니다.
+        abstract class TestMutexService: BuildService<BuildServiceParameters.None>
+
+        val testMutex = gradle.sharedServices.registerIfAbsent(
+            "test-mutex",
+            TestMutexService::class
+        ) {
+            maxParallelUsages.set(1)
+        }
+
         test {
+            // 멀티 모듈들을 테스트 시에만 동시에 실행되지 않게 하기 위해 Mutex 를 활용합니다.
+            usesService(testMutex)
+
             useJUnitPlatform()
 
             // 테스트 시 아래와 같은 예외 메시지를 제거하기 위해서
