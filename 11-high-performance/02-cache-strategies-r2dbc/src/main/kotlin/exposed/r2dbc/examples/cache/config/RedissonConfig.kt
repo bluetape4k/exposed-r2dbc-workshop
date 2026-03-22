737ff59963e63fdd12874ae97ecc81075b3cb756
debop <sunghyouk.bae@gmail.com>
2026-03-22 19:29:22 +0900

    chore: RedissonClient 생성 시 warmup 추가

--- 11-high-performance/02-cache-strategies-r2dbc/src/main/kotlin/exposed/r2dbc/examples/cache/config/RedissonConfig.kt
+++ 11-high-performance/02-cache-strategies-r2dbc/src/main/kotlin/exposed/r2dbc/examples/cache/config/RedissonConfig.kt
@@ -1,15 +1,10 @@
 package exposed.r2dbc.examples.cache.config
 
-import io.bluetape4k.concurrent.virtualthread.VirtualThreadExecutor
 import io.bluetape4k.logging.coroutines.KLoggingChannel
-import io.bluetape4k.redis.redisson.codec.RedissonCodecs
 import io.bluetape4k.testcontainers.storage.RedisServer
-import org.redisson.Redisson
 import org.redisson.api.RedissonClient
-import org.redisson.config.Config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.time.Duration
 
 @Configuration
 class RedissonConfig {
@@ -21,27 +16,6 @@ class RedissonConfig {
 
     @Bean
     fun redissonClient(): RedissonClient {
-        val config = Config().apply {
-            useSingleServer()
-                .setAddress(redis.url)
-                .setConnectionPoolSize(128)
-                .setConnectionMinimumIdleSize(32) // 최소 연결을 충분히 확보하여 Latency 방지
-                .setIdleConnectionTimeout(10000)  // 연결 유지를 넉넉히 (10초)
-                .setTimeout(5000)
-                .setRetryAttempts(3)
-                .setRetryDelay { attempt -> Duration.ofMillis((attempt + 1) * 100L) }
-
-                .setDnsMonitoringInterval(5000)  // DNS 변경 감지 (Cloud 환경 필수)
-
-            executor = VirtualThreadExecutor
-            threads = 256
-            nettyThreads = 128
-            codec = RedissonCodecs.LZ4ForyComposite
-            setTcpNoDelay(true)
-            setTcpUserTimeout(5000)
-        }
-
-        return Redisson.create(config)
+        return RedisServer.Launcher.RedissonLib.getRedisson(redis.url)
     }
-
 }
